08a38cffbe493c5d422ed8a140a0c4a26bf21297
Mohamedbouoni <bouonymohamed@gmail.com>
2025-06-20 20:27:21 +0100

    Initial commit

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,68 @@
+'use client'
+import Image from 'next/image';
+import React from 'react'
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+const CountChart = () => {
+
+const data = [
+    {
+    name: 'Total',
+    count: 106,
+    fill: 'white',
+  },
+  {
+    name: 'Girls',
+    count: 50,
+    fill: '#FAE27C',
+  },
+  {
+    name: 'Boys',
+    count: 50,
+    fill: '#C3EBFA',
+  },
+];
+const style = {
+  top: '50%',
+  right: 0,
+  transform: 'translate(0, -50%)',
+  lineHeight: '24px',
+};
+  return (
+    <div className='bg-white rounded-xl w-full h-full'>
+        {/* Title */}
+        <div className='flex justify-between items-center'>
+            <h1 className='text-lg font-semibold'>Student</h1>
+            <Image src='/moreDark.png' alt='' height={20} width={20}  />
+        </div>
+        {/* CHART */}
+        <div className='relative w-full h-[75%]'>
+            <ResponsiveContainer >
+        <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
+          <RadialBar
+            
+            background
+            
+            dataKey="count"
+          />
+        </RadialBarChart>
+      </ResponsiveContainer>
+      <Image src='/maleFemale.png' alt='' width={50} height={50} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '/>
+        </div>
+        {/* BOTTOM */}
+        <div className='flex justify-center gap-16'>
+            <div className='flex flex-col gap-1'>
+                <div className='w-5 h-5 bg-lamaSky rounded-full'/>
+                <h1 className='font-bold'>1,234</h1>
+                <h3 className='text-xs text-gray-300'>Boys (55%)</h3>
+            </div>
+            <div className='flex flex-col gap-1'>
+                <div className='w-5 h-5 bg-lamaYellow rounded-full'/>
+                <h1 className='font-bold'>1,234</h1>
+                <h3 className='text-xs text-gray-300'>Girls (45%)</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CountChart
